88cb79092d77fb077a123edf641c6da7aee6523a
HowieHooo <howiehua.hh@gmail.com>
2017-10-09 03:05:05 -0700

    GTMD finally fix the image resizer and can upload the image to the firebase

--- app/ios/post.js
+++ app/ios/post.js
@@ -1,9 +1,14 @@
 import firebase from '../config/firebase';
 import Header from '../component/header';
 import styles from '../theme/theme.js';
-var Dimensions = require('Dimensions');
-var deviceWidth = Dimensions.get('window').width;
-var deviceHeight = Dimensions.get('window').height;
+import uploadImage from '../config/uploadImage';
+import ImagePicker from 'react-native-image-picker';
+import RNFetchBlob from 'react-native-fetch-blob';
+import ImageResizer from 'react-native-image-resizer';
+import Dimensions from 'Dimensions';
+const Blob = RNFetchBlob.polyfill.Blob
+const fs = RNFetchBlob.fs
+const deviceWidth = Dimensions.get('window').width;
 
 import React, { Component } from 'react';
 import {
@@ -18,11 +23,28 @@ class Post extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      image: 'https://firebasestorage.googleapis.com/v0/b/mealhunter-71d48.appspot.com/o/t440x300.jpg?alt=media&token=f150c8fc-3eb3-43b8-b4d3-d965f2e22bb2',
+      image: 'https://firebasestorage.googleapis.com/v0/b/mealhunter-71d48.appspot.com/o/default.jpg?alt=media&token=9f0fff61-5905-400f-8a10-222b2b07ac9b',
       place: ''
     };
   }
 
+photo(){
+  var state = this
+  window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
+  window.Blob = Blob
+  ImagePicker.showImagePicker({}, (response) => {
+    if(!response.didCancel){
+      const source = {uri: response.uri.replace('file://', ''), isStatic: true};
+      ImageResizer.createResizedImage(source.uri, 500, 500, 'JPEG', 60).then((resizedImageUri) => {
+        uploadImage(resizedImageUri)
+          .then(url => state.setState({ image: url }))
+          .catch(error => console.log(error))
+      }).catch((err) => {
+        console.log(err)
+      });
+    }
+  });
+}
 //create a ref called food and push the image and place under it as a object
   post(){
     firebase.database().ref('food').push({image: this.state.image, place: this.state.place});
@@ -40,7 +62,9 @@ class Post extends Component {
       <View >
         <Header title="Post" left={this.back.bind(this)} leftText={'Back'}/>
         <View style={ styles.center }>
-          <Image source={{uri: this.state.image}}  style={{ width: deviceWidth-10  , height: (deviceWidth*.6), borderRadius: 10}}/>
+          <TouchableOpacity onPress = {this.photo.bind(this)}>
+            <Image source={{uri: this.state.image}}  style={{ width: deviceWidth, height: (deviceWidth*.5)}}/>
+          </TouchableOpacity>
           <TextInput
             style={styles.textInput}
             //the length of the placeholder will be the same length that you can see what your typing?

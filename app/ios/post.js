f1025bbc46b3c8972eea7c4ef994d4449b2fa289
HowieHooo <howiehua.hh@gmail.com>
2017-10-09 14:03:04 -0700

    add google place API to locate the food

--- app/ios/post.js
+++ app/ios/post.js
@@ -9,6 +9,7 @@ import Dimensions from 'Dimensions';
 const Blob = RNFetchBlob.polyfill.Blob
 const fs = RNFetchBlob.fs
 const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
 
 import React, { Component } from 'react';
 import {
@@ -16,7 +17,8 @@ import {
   Text,
   TouchableOpacity,
   TextInput,
-  Image
+  Image,
+  ScrollView
 } from 'react-native';
 
 class Post extends Component {
@@ -24,27 +26,49 @@ class Post extends Component {
     super(props);
     this.state = {
       image: 'https://firebasestorage.googleapis.com/v0/b/mealhunter-71d48.appspot.com/o/default.jpg?alt=media&token=9f0fff61-5905-400f-8a10-222b2b07ac9b',
-      place: ''
+      place: '',
+      latitude: '',
+      longtitude: '',
+      nearby: []
     };
   }
 
-photo(){
-  var state = this
-  window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
-  window.Blob = Blob
-  ImagePicker.showImagePicker({}, (response) => {
-    if(!response.didCancel){
-      const source = {uri: response.uri.replace('file://', ''), isStatic: true};
-      ImageResizer.createResizedImage(source.uri, 500, 500, 'JPEG', 60).then((resizedImageUri) => {
-        uploadImage(resizedImageUri)
-          .then(url => state.setState({ image: url }))
-          .catch(error => console.log(error))
-      }).catch((err) => {
-        console.log(err)
-      });
-    }
-  });
-}
+
+  componentDidMount(){
+    this.getLocations();
+  }
+
+  getLocations(){
+    navigator.geolocation.getCurrentPosition(
+      (position) =>{
+        const coords = position.coords.latitude + ',' + position.coords.longitude
+        const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + coords + '&radius=2000&type=restaurant&keyword=cruise&key=AIzaSyAN6P_a6v3ZuXQYEa7RjLivMogbK7ck3Jo'
+        fetch(url, {method: "GET"})
+        .then((response) => response.json())
+        .then((responseData) => {
+          this.setState({nearby : responseData.results })
+        })
+      }
+    )
+  }
+//first use imagepicker to call the native prompts, then use imagreresizer to resize the image and then use image fetch blob to upload the image into firebase
+  photo(){
+    var state = this
+    window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
+    window.Blob = Blob
+    ImagePicker.showImagePicker({}, (response) => {
+      if(!response.didCancel){
+        const source = {uri: response.uri.replace('file://', ''), isStatic: true};
+        ImageResizer.createResizedImage(source.uri, 500, 500, 'JPEG', 60).then((resizedImageUri) => {
+          uploadImage(resizedImageUri)
+            .then(url => state.setState({ image: url }))
+            .catch(error => console.log(error))
+        }).catch((err) => {
+          console.log(err)
+        });
+      }
+    });
+  }
 //create a ref called food and push the image and place under it as a object
   post(){
     firebase.database().ref('food').push({image: this.state.image, place: this.state.place});
@@ -72,6 +96,16 @@ photo(){
             onChangeText={(place) => this.setState({place: place})}
             value={this.state.place}/>
           <View style={styles.line} />
+          <ScrollView style= {{height: deviceHeight*0.3, width: deviceWidth-10}} >
+            {Object.keys(this.state.nearby).map((key) => {
+              return(
+                <TouchableOpacity style={{padding : 10, margin: -1}} onPress={(place) =>  this.setState({place: this.state.nearby[key].name})}>
+                  <Text style={styles.text}>{this.state.nearby[key].name}</Text>
+                  <Text style={styles.text}>{this.state.nearby[key].vicinity}</Text>
+                </TouchableOpacity>
+              )
+            })}
+          </ScrollView>
           <TouchableOpacity style={ styles.btn } onPress={this.post.bind(this)}>
             <Text style={ styles.text }>Post</Text>
           </TouchableOpacity>
